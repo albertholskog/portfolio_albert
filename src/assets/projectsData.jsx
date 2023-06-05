b0d1fcffb2bb512891016d3e4c3aeb3f953ba485
albertholskog <91052944+albertholskog@users.noreply.github.com>
2023-06-05 22:48:58 +0200

    new fix

---
+++ src/assets/projectsData.jsx
@@ -0,0 +1,90 @@
+
+import Holidaze from "../assets/projectsImages/holidazeHome.png";
+import Bidit from "../assets/projectsImages/bidit.png";
+import BeardLand from "../assets/projectsImages/beardLand.png";
+import Gamehub from "../assets/projectsImages/gamehub.png";
+import Community from "../assets/projectsImages/community.png";
+import SkylineShop from "../assets/projectsImages/skylineShop.png";
+
+import CssIcon from "../assets/icon/CssIcon";
+import BootstrapIcon from "../assets/icon/BootstrapIcon";
+import CypressIcon from "../assets/icon/CypressIcon";
+import HtmlIcon from "../assets/icon/HtmlIcon";
+import JavascriptIcon from "../assets/icon/JavascriptIcon";
+import JestIcon from "../assets/icon/JestIcon";
+import MuiIcon from "../assets/icon/MuiIcon";
+import ReactIcon from "../assets/icon/ReactIcon";
+import ReduxIcon from "../assets/icon/ReduxIcon";
+import SassIcon from "../assets/icon/SassIcon";
+
+const projectsData = [
+  {
+    name: "Holidaze",
+    image: Holidaze,
+    githubLink: "https://github.com/albertholskog/Holidaze_eksamen",
+    description: "test",
+    demoLink: "https://roaring-hummingbird-c27e57.netlify.app/",
+    icons: [
+      <ReactIcon key="react" size={20} />,
+      <MuiIcon key="mui" size={20} />,
+      <ReduxIcon key="redux" size={20} />,
+      <JestIcon key="jest" size={20} />,
+    ],
+  },
+  {
+    name: "SkylineShop",
+    image: SkylineShop,
+    githubLink: "https://github.com/albertholskog/CA_React",
+    description: "test",
+    demoLink: "https://bright-hotteok-d98ecd.netlify.app/",
+    icons: [
+      <ReactIcon key="react" size={20} />,
+      <CssIcon key="Css" size={20} />,
+      <ReduxIcon key="redux" size={20} />,
+    ],
+  },
+  // {
+  //   name: "Bid:it",
+  //   image: Bidit,
+  //   githubLink: "https://github.com/albertholskog/Semester-Project-2",
+  //   description: "test",
+  //   demoLink: "https://bright-hotteok-d98ecd.netlify.app/",
+  //   icons: [
+  //     <JavascriptIcon key="Javascript"size={20} />,
+  //     <BootstrapIcon key="Bootstrap" size={20}/>,
+  //     <SassIcon key="Sass" size={20}/>,
+  //     <CypressIcon key="Cypress" size={20}/>,
+  //     <JestIcon key="jest" size={20}/>,
+  //   ],
+  // },
+  // {
+  //   name: "BeardLand",
+  //   image: BeardLand,
+  //   githubLink: "https://github.com/username/project2",
+  //   description: "test",
+  //   demoLink: "",
+  //   icons: [
+  //     <JavascriptIcon key="Javascript"size={20} />,
+  //     <HtmlIcon key="Html" size={20}/>,
+  //     <CssIcon key="Css" size={20}/>,
+  //   ],
+  // },
+  // {
+  //   name: "Gamehub",
+  //   image: Gamehub,
+  //   githubLink: "https://github.com/albertholskog/emmneoppgave-gamehub",
+  //   description: "test",
+  //   demoLink: "https://sharp-northcutt-2538a8.netlify.app/",
+  //   icons: [<HtmlIcon key="Html" size={20} />, <CssIcon key="Css" size={20}/>],
+  // },
+  // {
+  //   name: "Community Science Museum",
+  //   image: Community,
+  //   githubLink: "https://github.com/albertholskog/semesteroppgave-semester1",
+  //   description: "test",
+  //   demoLink: "https://gracious-mahavira-3568d2.netlify.app/index.html",
+  //   icons: [<HtmlIcon key="Html" size={20}/>, <CssIcon key="Css" size={20}/>],
+  // },
+];
+
+export default projectsData;
